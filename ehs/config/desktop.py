394ca5747047f2039bb8b0bcdee1d011470f0609
pgoret <p.goret@caconsultants.be>
2025-11-26 09:24:58 +0100

    fix: align module name for EHS

--- ehs/config/desktop.py
+++ ehs/config/desktop.py
@@ -3,10 +3,10 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "ehs",
+			"module_name": "EHS",
 			"color": "red",
 			"icon": "heart-active",
 			"type": "module",
-			"label": _("ehs")
+			"label": _("EHS")
 		}
 	]
